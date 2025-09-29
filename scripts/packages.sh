9f98e0049e120985d032f4138483f8d088a38186
xinruoyusixian <xinruoyusixian@163.com>
2025-09-29 05:07:56 +0000

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -46,24 +46,11 @@ UPDATE_PACKAGE() {
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "chingjyu/luci-theme-argon" "main"
-
-UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "master"
-UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
-UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
-UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
-UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
-
-UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-UPDATE_PACKAGE "openlist" "sbwml/luci-app-openlist" "main"
 UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
 UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
 UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
-UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
-UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "netspeedtest" "sirpdboy/luci-app-netspeedtest" "js" "" "homebox speedtest"
+
 
 
 #流量监控插件
@@ -81,8 +68,7 @@ UPDATE_PACKAGE "luci-app-autorepeater" "kenzok8/small-package" "main"
 UPDATE_PACKAGE "luci-app-ddnsto" "kenzok8/small-package" "main"
 UPDATE_PACKAGE "luci-app-unishare" "kenzok8/small-package" "main"
 UPDATE_PACKAGE "luci-app-watchdog" "kenzok8/small-package" "main"
-UPDATE_PACKAGE "luci-app-udp2raw" "kenzok8/small-package" "main"
-#UPDATE_PACKAGE "luci-app-tinyfilemanager" "kenzok8/small-package" "main"
+UPDATE_PACKAGE "luci-app-tinyfilemanager" "kenzok8/small-package" "main"
 UPDATE_PACKAGE "luci-app-timecontrol" "kenzok8/small-package" "main"
 UPDATE_PACKAGE "luci-app-syncdial" "kenzok8/small-package" "main"
 #UPDATE_PACKAGE "luci-app-oaf" "kenzok8/small-package" "main"
